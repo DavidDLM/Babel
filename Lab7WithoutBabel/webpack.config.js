b76c741cb6dccaeeac7e04a22aed609377e23ba7
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2022-03-15 22:06:54 -0600

    Add files via upload

---
+++ Lab7WithoutBabel/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: false,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'bundle'),
+    filename: 'main.bundle.js',
+  },
+  plugins: [new CleanWebpackPlugin(), 
+    new HtmlWebpackPlugin({
+        template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({filename: "main.bundle.css"})],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|mp3|mp4)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'images',
+        },
+      }
+    ],
+  },
+};
